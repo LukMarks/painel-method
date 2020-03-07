3e64612537bddeec38aea6eed28014e4df8a5af5
LukMarks <lucas.mmoreno@hotmail.com>
2020-03-07 15:44:52 -0300

    Initial commit

---
+++ painel_method.py
@@ -0,0 +1,10 @@
+# # Author: Lucas Marques Moreno
+# Date: 07/03/2020
+# Description: Python's implementations
+# of the painel method
+
+class painel_method():
+    def __init__:
+        pass
+        return
+    
